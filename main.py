6f4be89a45a60942213935261159e051c97ef4e7
hiseyong <hiseyong@naver.com>
2026-02-12 21:04:27 +0900

    login functionalized

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ class SSLAdapter(HTTPAdapter):
         self.poolmanager = PoolManager(*args, **kwargs)
 
 #이부분을 수정할것
-date = [20251205]  # 날짜 YYYYMMDD 형식으로 작성, 콤마로 구분
-court = [1]  # 코트번호, 콤마로 구분
-starttime = [14]  # 시작하는 시각을 24시간 형식(오후 1시 = 13)으로 작성, 콤마로 구분
+date = [20260205,20260227,20260220,20260219,20260212,20260211,20260204]  # 날짜 YYYYMMDD 형식으로 작성, 콤마로 구분
+court = [2,2,2,2,2,2,2]  # 코트번호, 콤마로 구분
+starttime = [10,10,10,10,8,10,10]  # 시작하는 시각을 24시간 형식(오후 1시 = 13)으로 작성, 콤마로 구분
 
 
 #이 아래는 수정 금지
@@ -24,25 +24,18 @@ async def main():
     session = requests.Session()
     session.mount("https://", SSLAdapter())
 
+    login(session)
+
+    wait_until_open(date[0], session)
+    datels = await make_data(date, court, starttime)
+    print(datels)
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
         'Origin': 'https://yeyak.gys.or.kr',
         'Referer': 'https://yeyak.gys.or.kr/'
     }
-    res = session.get('https://yeyak.gys.or.kr/fmcs/27?referer=https%3A%2F%2Fgbc.gys.or.kr%2F&login_check=skip',
-                      headers=headers)
-    security_token = res.text.split("SecurityToken")[1].split("value=")[1].split("/>")[0].replace('"', '')
-    print(security_token)
-    session.post('https://yeyak.gys.or.kr/fmcs/27?referer=https://gbc.gys.or.kr/&login_check=skip', headers=headers,
-                 data={"userId": "hiseyong", "userPassword": "13579013579ahn", "SecurityToken": security_token})
-    session.post('https://www.gys.or.kr/member/SSO/newlogin', headers=headers,
-                 data={"memid": "hiseyong", "memno": "420117", "returl": "https://gbc.gys.or.kr/"})
-    session.get(
-        'https://gbc.gys.or.kr:446/member/sso_login_process.php?memid=hiseyong&memno=420177&returl=https://gbc.gys.or.kr:446/rent/tennis_rent.php?part_opt=07',
-        headers=headers)
-    wait_until_open(date[0], session)
-    datels = await make_data(date, court, starttime)
-    print(datels)
+
     for i in datels:
         prereq = session.post('	https://gbc.gys.or.kr:446/rent/rent_period_apply-1.php', headers=headers, data=i)
         respon = session.post('https://gbc.gys.or.kr:446/rent/rent_period_proc.php', headers=headers, data=i)
--- utils.py
+++ utils.py
@@ -2,28 +2,48 @@ import requests
 def wait_until_open(date, session):
     print("사이트 오픈 대기중...")
     while True:
-        data = {
-            'rent_date': str(date),
-            'nyear': str(date)[:4],
-            'nmonth': str(date)[4:6],
-            'nday': str(date)[6:8],
-            'stime': '',
-            'etime': '',
-            'place_opt': "6",
-            'place_nm': f"테니스 {6}코트".encode(encoding='cp949'),
-            'rent_gubun': '1001',
-        }
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-            'Origin': 'https://yeyak.gys.or.kr',
-            'Referer': 'https://yeyak.gys.or.kr/'
-        }
-        #session = requests.Session()
-        #session.get(
-            # 'https://gbc.gys.or.kr:446/member/sso_login_process.php?memid=hiseyong&memno=420177&returl=https://gbc.gys.or.kr:446/rent/tennis_rent.php?part_opt=07',
-            # headers=headers)
-        res = session.post('https://gbc.gys.or.kr:446/rent/tennis_rent.php?part_opt=07', data=data, headers=headers)
-        # print(res.content.decode('cp949'))
-        # if "대관 신청접수 기간이 아닙니다" in res.content.decode('cp949'): continue
-        if "대관 신청접수 기간이 아닙니다" not in res.content.decode('cp949'):
-            break
+        try:
+            data = {
+                'rent_date': str(date),
+                'nyear': str(date)[:4],
+                'nmonth': str(date)[4:6],
+                'nday': str(date)[6:8],
+                'stime': '',
+                'etime': '',
+                'place_opt': "6",
+                'place_nm': f"테니스 {6}코트".encode(encoding='cp949'),
+                'rent_gubun': '1001',
+            }
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+                'Origin': 'https://yeyak.gys.or.kr',
+                'Referer': 'https://yeyak.gys.or.kr/'
+            }
+
+            login(session)
+
+            res = session.post('https://gbc.gys.or.kr:446/rent/tennis_rent.php?part_opt=07', data=data, headers=headers)
+            # print(res.content.decode('cp949'))
+            # if "대관 신청접수 기간이 아닙니다" in res.content.decode('cp949'): continue
+            if "대관 신청접수 기간이 아닙니다" not in res.content.decode('cp949'):
+                break
+        except:
+            continue
+
+def login(session):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Origin': 'https://yeyak.gys.or.kr',
+        'Referer': 'https://yeyak.gys.or.kr/'
+    }
+    res = session.get('https://yeyak.gys.or.kr/fmcs/27?referer=https%3A%2F%2Fgbc.gys.or.kr%2F&login_check=skip',
+                      headers=headers)
+    security_token = res.text.split("SecurityToken")[1].split("value=")[1].split("/>")[0].replace('"', '')
+    print(security_token)
+    session.post('https://yeyak.gys.or.kr/fmcs/27?referer=https://gbc.gys.or.kr/&login_check=skip', headers=headers,
+                 data={"userId": "hiseyong", "userPassword": "13579013579ahn", "SecurityToken": security_token})
+    session.post('https://www.gys.or.kr/member/SSO/newlogin', headers=headers,
+                 data={"memid": "hiseyong", "memno": "420117", "returl": "https://gbc.gys.or.kr/"})
+    session.get(
+        'https://gbc.gys.or.kr:446/member/sso_login_process.php?memid=hiseyong&memno=420177&returl=https://gbc.gys.or.kr:446/rent/tennis_rent.php?part_opt=07',
+        headers=headers)
